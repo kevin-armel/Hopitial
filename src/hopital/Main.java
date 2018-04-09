eb87152bba80d6b917e893639272fcae187fb247
kevin-armel <kevin-armel.sonkeng@edu.ece.fr>
2018-04-09 16:19:38 +0200

    Initialisation du projet.

---
+++ src/hopital/Main.java
@@ -0,0 +1,17 @@
+
+package hopital;
+
+/**
+ *
+ * @author Armel-PC
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
