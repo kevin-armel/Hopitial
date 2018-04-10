a0bf9d4e79baf19f448e77045211d175677a8bfb
PC-Antoine\Antoine <antoinelaguarigue@hotmail.fr>
2018-04-10 16:32:19 +0200

    Test modif

--- src/hopital/Main.java
+++ src/hopital/Main.java
@@ -12,6 +12,8 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Hello le monde Java :");
+        System.out.println("antoine");
+        
     }
     
 }
